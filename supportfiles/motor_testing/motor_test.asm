17eea4a6423cab2f0a0aa3e29e9708affa3062c4
todoesverso <todoesverso@cd9a0b1b-0856-0410-84ca-fb89ec2eb5fc>
2009-01-09 23:47:51 +0000

    Deleted all non-needed files

--- supportfiles/motor_testing/motor_test.asm
+++ supportfiles/motor_testing/motor_test.asm
@@ -5,7 +5,7 @@
 ;############################################################
         list            p=pic16f877a
         include         p16f877a.inc
-        __config _hs_osc & _wdt_off & _pwrte_on & _cp_off
+        __config _HS_OSC & _WDT_OFF & _PWRTE_ON & _CP_OFF
         errorlevel      -302    ;para el msg de bank warning
 
 ;###############  Definicion de etiquetas  ############### 
@@ -28,41 +28,41 @@ rb7     equ     7               ;B7
         org     0               ;restablecer vector
         goto    init
         org     4               ;vector interrupcion
-        clrf    intcon          ;reestablecer reg interruption
+        clrf    INTCON          ;reestablecer reg interruption
 
 ;###############      Proceso inicial      ############### 
 init
-        bsf     status,rp0      ;cambiar a bank1
-        clrf    trisa           ;establecer portA en OUT
+        bsf     STATUS,RP0      ;cambiar a bank1
+        clrf    TRISA           ;establecer portA en OUT
         movlw   b'00100111'     ;b0,1,2,5=IN b7=OUT
-        movwf   trisb           ;establecer portB
+        movwf   TRISB           ;establecer portB
         movlw   b'10000000'     ;bpu=1 pullup no usado
-        movwf   option_reg      ;establecer option_reg
-        bcf     status,rp0      ;cambiar a bank0
+        movwf   OPTION_REG      ;establecer OPTION_REG
+        bcf     STATUS,RP0      ;cambiar a bank0
         clrf    mode            ;establecer modo=PARAR 
         clrf    cuenta1         ;limpiar contador
         clrf    cuenta2         ;limpiar contador
         movlw   b'00000101'     ;establecer portA a valor inicial
-        movwf   porta           ;escribir portA
-        bsf     portb,rb7       ;establecer b7=1
-        btfsc   portb,rb5       ;b5=0 ?
+        movwf   PORTA           ;escribir portA
+        bsf     PORTB,rb7       ;establecer b7=1
+        btfsc   PORTB,rb5       ;b5=0 ?
         goto    $-1             ;no.  esperar
 
 start
 
 ;###############    Verificar condicion    ############### 
-        btfsc   portb,rb1       ;B1(PARAR) = ON ?
+        btfsc   PORTB,rb1       ;B1(PARAR) = ON ?
         goto    verificar1      ;no.  siguiente
         clrf    mode            ;si. establecer PARAR modo
         goto    drive           ;no.  saltar a motor drive
 verificar1
-        btfsc   portb,rb2       ;B2(DER) = ON ?
+        btfsc   PORTB,rb2       ;B2(DER) = ON ?
         goto    verificar2      ;no.  siguiente
         movlw   d'1'            ;si. establecer modo derecha 
         movwf   mode            ;grabar modo
         goto    drive           ;no.  saltar a motor drive
 verificar2
-        btfsc   portb,rb0       ;B0(IZQ) = ON ?
+        btfsc   PORTB,rb0       ;B0(IZQ) = ON ?
         goto    drive           ;no.  saltar a motor drive
         movlw   d'2'            ;si. establecer modo izquierda 
         movwf   mode            ;grabar modo
@@ -71,18 +71,18 @@ verificar2
 drive
         movf    mode,w          ;leer modo
         bz      start           ;modo = PARAR
-        bsf     portb,rb7       ;establecer B7 = 1
-        btfsc   portb,rb5       ;B5 = 0 ?
+        bsf     PORTB,rb7       ;establecer B7 = 1
+        btfsc   PORTB,rb5       ;B5 = 0 ?
         goto    $-1             ;no.  esperar
         movlw   d'5'            ;establecer loop cuenta(5msec)
         movwf   cuenta1         ;grabar loop cuenta
 loop    call    timer           ;esperar 1msec
         decfsz  cuenta1,f       ;cuenta - 1 = 0 ?
         goto    loop            ;no.  continuar
-        bcf     portb,rb7       ;establecer B7 = 0
-        btfss   portb,rb5       ;B5 = 1 ?
+        bcf     PORTB,rb7       ;establecer B7 = 0
+        btfss   PORTB,rb5       ;B5 = 1 ?
         goto    $-1             ;no.  esperar
-        movf    porta,w         ;leer portA
+        movf    PORTA,w         ;leer portA
         sublw   b'000000101'    ;verificar motor  
         bnz     drive2          ;proximo
         movf    mode,w          ;leer modo
@@ -95,7 +95,7 @@ drive1
         goto    drive_end       ;saltar a portA  
 ;-------------------------------------------------
 drive2
-        movf    porta,w         ;leer portA
+        movf    PORTA,w         ;leer portA
         sublw   b'000000110'    ;verificar motor  
         bnz     drive4          ;proximo
         movf    mode,w          ;leer modo
@@ -108,7 +108,7 @@ drive3
         goto    drive_end       ;saltar a portA  
 ;-------------------------------------------------
 drive4
-        movf    porta,w         ;leer portA
+        movf    PORTA,w         ;leer portA
         sublw   b'000001010'    ;verificar motor  
         bnz     drive6          ;proximo
         movf    mode,w          ;leer modo
@@ -121,7 +121,7 @@ drive5
         goto    drive_end       ;saltar a portA  
 ;-------------------------------------------------
 drive6
-        movf    porta,w         ;leer portA
+        movf    PORTA,w         ;leer portA
         sublw   b'000001001'    ;verificar motor  
         bnz     drive8          ;proximo
         movf    mode,w          ;leer modo
@@ -137,7 +137,7 @@ drive8
         movlw   b'00000101'     ;establecer est inicial
 
 drive_end
-        movwf   porta           ;escribir portA
+        movwf   PORTA           ;escribir portA
         goto    start           ;saltar a inicio
 ;###############   ############### 
 ;#############  1msec Timer Subroutine  #################
