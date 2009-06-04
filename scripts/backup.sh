3f2c4e5af1f526beb20a2e67376dbbcceb74d6fe
todoesverso <todoesverso@cd9a0b1b-0856-0410-84ca-fb89ec2eb5fc>
2009-06-04 00:55:55 +0000

    Committed new scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,9 +5,9 @@
 # Genera un tgz del directorio ../trunk (con export, osea que no es una 
 # working copy)  y luego hace un commit a ../tags/trunk.tgz
 
-svn export ../trunk trunk             ;
-tar cvzf ../tags/trunk.tgz trunk/     ;
-rm -fr trunk/ 			      ;
-svn commit ../tags/trunk.tgz -m "Commit generado por backup.sh (`date`)";
+svn export ../ complete             ;
+tar cvzf ../tags/complete.tgz complete/     ;
+rm -fr complete/ 			      ;
+svn commit ../tags/complete.tgz -m "Commit generado por backup.sh (`date`)";
 
 exit 0
