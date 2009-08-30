03313a4daf97225e394d209fd3c636bc9567387d
todoesverso <todoesverso@cd9a0b1b-0856-0410-84ca-fb89ec2eb5fc>
2009-08-30 02:57:30 +0000

    Code style updated

--- trunk/firmware/main.c
+++ trunk/firmware/main.c
@@ -24,11 +24,18 @@
 // Constantes definidas por el tamaño de la pagina Braille
 //#define BYTESXLIN 3
 #define NUMLINES 7
-// Instruction codes from the host 
+
+/** 
+ * Instruction codes from the host 
+ **/
 #define RESET           0x00
 #define PRINT           0x01
 #define MOV_SHORT       0x02
 #define MOV_LONG        0x03
+#define ROT_SHORT       0x04
+#define ROT_LONG        0x05
+#define PULL_PAPER      0x06
+#define STATUS          0x07
 // Codigo para tipo de movimiento vertical
 #define SHORT_OUT 1
 #define LONG_OUT 2
@@ -70,22 +77,22 @@
 //   26 mA @ 24 MHz
 //   35 mA @ 48 MHz
 //
-// Definición de los registros de configuración de fusibles (fuses)
-#if defined(pic18f2550) || defined(pic18f2455) || defined(pic18f4550) || defined(pic18f4455)
-code char at 0x300000 CONFIG1L = 0x20; // CPU system clock 96MHz PLL div 2, Oscillator No div (4MHz input)
-code char at 0x300001 CONFIG1H = 0x0e; // HS oscillator, PLL enabled, HS used by USB
-code char at 0x300002 CONFIG2L = 0x20; // Brown out off, PWRT On 
-code char at 0x300003 CONFIG2H = 0x00; // WDT off
-code char at 0x300004 CONFIG3L = 0xff; // Unused configuration bits
-code char at 0x300005 CONFIG3H = 0x81; // MCLR, PORTB digital, CCP2 - RC1
-code char at 0x300006 CONFIG4L = 0x80; // ICD off, ext off, LVP off, stk ovr off
-code char at 0x300007 CONFIG4H = 0xff; // Unused configuration bits
-code char at 0x300008 CONFIG5L = 0xff; // No code read protection
-code char at 0x300009 CONFIG5H = 0xff; // No data/boot read protection
-code char at 0x30000A CONFIG6L = 0xff; // No code write protection
-code char at 0x30000B CONFIG6H = 0xff; // No data/boot/table protection
-code char at 0x30000C CONFIG7L = 0xff; // No table read protection
-code char at 0x30000D CONFIG7H = 0xff; // No boot table protection
+// Definicion de los registros de configuracion de fusibles (fuses)
+#if defined(pic18f4550) 
+code char at 0x300000 CONFIG1L = 0x20; /* USB clk 96MHz PLL/2, PLL 4MHz */
+code char at 0x300001 CONFIG1H = 0x0e; /* HSPLL                         */
+code char at 0x300002 CONFIG2L = 0x20; /* USB regulator enable, PWRT On */ 
+code char at 0x300003 CONFIG2H = 0x00; /* Watchdog OFF                  */
+code char at 0x300004 CONFIG3L = 0xff; /*         ***UNUSED***          */
+code char at 0x300005 CONFIG3H = 0x81; /* MCLR enabled, PORTB digital   */
+code char at 0x300006 CONFIG4L = 0x80; /* all OFF except STVREN         */
+code char at 0x300007 CONFIG4H = 0xff; /*         ***UNUSED***          */
+code char at 0x300008 CONFIG5L = 0xff; /* No code read protection       */
+code char at 0x300009 CONFIG5H = 0xff; /* No data/boot read protection  */
+code char at 0x30000A CONFIG6L = 0xff; /* No code write protection      */
+code char at 0x30000B CONFIG6H = 0xff; /* No data/boot/table protection */
+code char at 0x30000C CONFIG7L = 0xff; /* No table read protection      */
+code char at 0x30000D CONFIG7H = 0xff; /* No boot table protection      */
 #endif
 
 // Buffers usados para el Endpoint1 (convencionalmente adoptado como Bus de datos)
@@ -165,41 +172,45 @@ void move(byte loops, byte direction, byte motor)
                 }
 }
 
-void mov_paper (byte steps) {
+void mov_paper(byte steps) 
+{
  move(steps, ARR, RODILLO);
 }
 
-void reset_carro(void){
-  while(SENS_CARRO)
-    move(1, IZQ, CARRO);
- move(8, DER, CARRO); // Esto es una sangria
-
+void reset_carro(void)
+{
+        while (SENS_CARRO)
+                move(1, IZQ, CARRO);
+        move(8, DER, CARRO); // Esto es una sangria
 }
 
-void golpear(void){
- delay(100);
- PERCUTOR = 1;
- delay(100);
- PERCUTOR = 0;
- delay(100);
+void golpear(void)
+{
+        delay(100);
+        PERCUTOR = 1;
+        delay(100);
+        PERCUTOR = 0;
+        delay(100);
 }
 
-byte check_bit(byte byte_in, byte pos){
+byte check_bit(byte byte_in, byte pos)
+{
 // Recibe como parametro un byte y un indice para el byte.
 // Devuelve 1 si el bit del indice esta en uno o 0 si esta en cero.
 //  (byte, [0-7])
- byte mascara;
- mascara = 0x01; // Para la lectura del cada bit
- mascara = mascara << pos;
-   if (byte_in & mascara)
-     return 1;
- return 0;
+        byte mascara;
+        mascara = 0x01; // Para la lectura del cada bit
+        mascara = mascara << pos;
+        if (byte_in & mascara)
+                return 1;
+        return 0;
 }
 
-void set_bit(byte *byte_in, byte pos){
- byte mask = 0x01, byte_aux = *byte_in;
- mask = mask << pos;
- *byte_in = byte_aux ^ mask; 
+void set_bit(byte *byte_in, byte pos)
+{
+        byte mask = 0x01, byte_aux = *byte_in;
+        mask = mask << pos;
+        *byte_in = byte_aux ^ mask; 
  /* This could be a simple | too, but ^ makes it better
   * because it would destroy the byte if something goes wrong. 
   * Let's say that byte_in = 0x07 (0000 0111), and for some 
@@ -209,38 +220,41 @@ void set_bit(byte *byte_in, byte pos){
   */                         
 }
 
-byte print_byte(byte *p){
- byte a, i, byte_in, byte_ctl = 0x00;
- byte_in = *p;
-   for (i = 8; i > 0; i--) {
-     if (check_bit(byte_in, i-1)){ 
-       golpear();
-       set_bit(&byte_ctl, i-1); // Recreate the byte for error control
-     }
-       a =(byte)i;
+byte print_byte(byte *p)
+{
+        byte a, i, byte_in, byte_ctl = 0x00;
+        byte_in = *p;
+        for (i = 8; i > 0; i--) {
+                if (check_bit(byte_in, i-1)){ 
+                        golpear();
+                set_bit(&byte_ctl, i-1); // Recreate the byte for error control
+                }
+        a = (byte)i;
  // Movimiento del carro segun la posicion par-impar del braille dot
-     if (!(a&1)) // Chequea la paridad (PAR = minima sep, IMPAR = Maxima sep)
-       move(2, DER, CARRO); // Separacion horizontal mínima del caracter braille
-     else
-       move(4, DER, CARRO); // Separacion horizontal máxima del caracter braille
-    }
-   return byte_ctl;
+        if (!(a&1)) // Chequea la paridad (PAR = minima sep, IMPAR = Maxima sep)
+                move(2, DER, CARRO); // Separacion horizontal mínima del caracter braille
+        else
+                move(4, DER, CARRO); // Separacion horizontal máxima del caracter braille
+        }
+        return byte_ctl;
 }
 
-void print_line(byte *p, byte *e){
- byte width_b;
- reset_carro();
- width_b = NUMLINES - 1;
-   while (width_b) {
-     *e = print_byte(p);
-     p++;
-     e++;
-     width_b--;
-   }
+void print_line(byte *p, byte *e)
+{
+        byte width_b;
+        reset_carro();
+        width_b = NUMLINES - 1;
+        while (width_b) {
+                *e = print_byte(p);
+                p++;
+                e++;
+                width_b--;
+        }
 }
 
-static void USB(void){
- byte rxCnt;
+static void USB(void)
+{
+        byte rxCnt;
  /* The code should present a clear distiction between
   * data direction
   */
@@ -248,27 +262,24 @@ static void USB(void){
  /* 
   * Find out if an output request has arrived to EP2
   */
- rxCnt = BulkOut(2, &instruction, 1); 
-   if (rxCnt == 0) return;
+        rxCnt = BulkOut(2, &instruction, 1); 
+        if (rxCnt == 0) return;
    //Now rxBuffer[0] is actually the instruction that follow
  
   // Interpret instructions recived from host
-  if (instruction == MOV_SHORT) {
- 	mov_paper (SHORT_STEPS_OUT); 
-  }
-  else if (instruction == MOV_LONG){
-	mov_paper (LONG_STEPS_OUT);
-  }
-
-  else if (instruction == PRINT) {
-   do{
+        if (instruction == MOV_SHORT) {
+                mov_paper (SHORT_STEPS_OUT); 
+        } else if (instruction == MOV_LONG){
+	        mov_paper (LONG_STEPS_OUT);
+        } else if (instruction == PRINT) {
+                do{
   // *** HOST --> DEVICE  ***
-      rxCnt = BulkOut(1, rxBuffer, INPUT_BYTES); // Shuld this be OUTP?
-   } while (rxCnt == 0);
-        print_line(rxBuffer, echoVector);
+                        rxCnt = BulkOut(1, rxBuffer, INPUT_BYTES); // Shuld this be OUTP?
+                } while (rxCnt == 0);
+                print_line(rxBuffer, echoVector);
 
-           while (ep1Bi.Stat & UOWN)
-            ProcessUSBTransactions();
+                while (ep1Bi.Stat & UOWN)
+                ProcessUSBTransactions();
       /* We recived the order to PRINT, so we did it. 
        * Then we process usb transactions if any.
        * Now we are ready to send the data treated back
@@ -276,47 +287,49 @@ static void USB(void){
        * any errors.
        */
        // *** DEVICE --> HOST ***
-       do{ 
-         rxCnt =  BulkIn(1, echoVector, INPUT_BYTES);
-       } while(rxCnt == 0);
+                do { 
+                        rxCnt = BulkIn(1, echoVector, INPUT_BYTES);
+                } while (rxCnt == 0);
 
-  }
-   else if (instruction == RESET)
-        reset_carro();
+        } else if (instruction == RESET)
+                reset_carro();
    
-      while (ep1Bi.Stat & UOWN)
-            ProcessUSBTransactions();
+        while (ep1Bi.Stat & UOWN)
+                  ProcessUSBTransactions();
 
         apagar_motores();
 }
 
-void ProcessIO(void){	
+void ProcessIO(void) 
+{	
 // Tareas de aplicación de Usuario USB
-  if ((deviceState < CONFIGURED) || (UCONbits.SUSPND==1))
-    return;
-  USB();
+        if ((deviceState < CONFIGURED) || (UCONbits.SUSPND==1))
+                return;
+        USB();
 }
 
 // Punto de entrada del Firmware
-void main(void){
+void main(void)
+{
  // LLamada a la función de inicialización de usuario
- UserInit();
+        UserInit();
  // Inicializar USB
- UCFG = 0x14; // Habilita las resistencias de pullup; modo full speed
+        UCFG = 0x14; // Habilita las resistencias de pullup; modo full speed
 
  // Condiciones iniciales del dispositivo
- deviceState = DETACHED;
- remoteWakeup = 0x00;
- currentConfiguration = 0x00;
- apagar_motores();
-   while(1){
+        deviceState = DETACHED;
+        remoteWakeup = 0x00;
+        currentConfiguration = 0x00;
+        apagar_motores();
+
+        while(1) {
    // Asegurar que el modulo USB está disponible
-     EnableUSBModule();
+                EnableUSBModule();
     // En cuanto sale del modo de prueba (UTEYE), procesa
     // las transactiones USB
-       if(UCFGbits.UTEYE != 1)
-         ProcessUSBTransactions();
+                if(UCFGbits.UTEYE != 1)
+                        ProcessUSBTransactions();
         // Tareas de apliacación específica
-      ProcessIO();
-   }
+                ProcessIO();
+        }
 }
